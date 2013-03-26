2ded22a3693121adb8f71fa537d57c74ba162a84
Seiichi Yonezawa <me@nzwsch.biz>
2013-03-26 05:10:35 -0700

    Add spec

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,15 @@
+require './fizzbuzz'
+
+def fizzbuzz(array=[*1..100])
+  array.each_with_index do |num, index|
+    array[index] = 'Fizz' if num % 3 == 0
+    array[index] = 'Buzz' if num % 5 == 0
+    array[index] = 'FizzBuzz' if num % 3 == 0 && num % 5 == 0
+  end
+end
+
+describe "FizzBuzz" do
+  it "same result at original" do
+    pp fizzbuzz
+  end
+end
