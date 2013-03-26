c9d0da533b1e775541dfd625189906bb3e012aee
Seiichi Yonezawa <me@nzwsch.biz>
2013-03-26 06:06:13 -0700

    Oneliner

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,1 @@
-class Fixnum
-  def fzbz
-    self.z(15,"FizzBuzz") || self.z(3,"Fizz") || self.z(5,"Buzz") || self
-  end
-  def z x,m; m if self.%(x).zero?; end
-end
-__FILE__ == $0 and 1.upto(100) {|i| puts i.fzbz }
+class Fixnum;def fzbz;self.z(15,"FizzBuzz")||self.z(3,"Fizz")||self.z(5,"Buzz")||self;end;def z x,m;m if self.%(x).zero?;end;end;1.upto(100){|i|puts i.fzbz} if __FILE__==$0
